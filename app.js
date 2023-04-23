8ec94dc0b3671cfa803a5e7343ccc5e70560bb2d
=armada6199 <moh.abdin27@gmail.com>
2023-04-23 23:57:48 +0300

    accounting

--- app.js
+++ app.js
@@ -1,40 +1,24 @@
-const data=[
-    {id:1000,fullName:"Ghazi Samer",department:"Administration",level:"Senior"},
-    {id:1002,fullName:"Tamara Ayoub",department:"Marketing",level:"Senior"},
-    {id:1003,fullName:"Safi Walid",department:"Administration",level:"Mid-Senior"},
-    {id:1004,fullName:"Omar Zaid",department:"Development",level:"Senior"},
-    {id:1005,fullName:"Rana Saleh",department:"Development",level:"Junior"},
-    {id:1006,fullName:"Hadi Ahmad",department:"Finance,",level:"Mid-Senior"},
-    {id:1001,fullName:"Lana Ali",department:"Finance",level:"Senior"},
-];
-let employeesArr;
 let employeeSection=document.querySelector(".main__employees");
 let form=document.getElementById("main__form");
 let cardsCont=document.getElementById("main__cards");
 let viewBtn=document.getElementById("view__btn");
-function getFromLocal(){
-    let data=JSON.parse(localStorage.getItem('employees'))
-    if(data!==null) employeesArr=data;
-    createCards(employeesArr)
-}
-getFromLocal()
+let employeesArr=[];
 function Employees(id,name,department,level,imgUrl="baseUrl"){
-    this.id=id;
+    this.id=id; 
     this.name=name;
     this.department=department;
     this.level=level;
     this.imgUrl=imgUrl;
-    this.salary;
-    this.calcSalary(this.level);
+    this.salary=0;
+    this.calcSalary(this.level)
 }
-
-Employees.prototype.calcSalary=function (level){
+Employees.prototype.calcSalary=function(level){
     switch (level){
-        case "Senior":{
+        case "senior":{
             this.salary=Math.floor((Math.random()*(2000-1500)+1500)*.7);
     break;
         }
-        case "Mid-Senior":{
+        case "mid-senior":{
             this.salary=Math.floor((Math.random()*(1500-1000)+1000)*.7);
             break;
         }
@@ -45,19 +29,31 @@ Employees.prototype.calcSalary=function (level){
         default :console.log(this.level);
     }
     }
+     function getFromLocal(){
+        let data=JSON.parse(localStorage.getItem('employees'))
+        if(data!==null) {
+            for(let i=0;i<data.length;i++){
+                let employee=new Employees(data[i].id,data[i].name,data[i].department,data[i].level,data[i].imgUrl)
+             employeesArr.push(employee)
+            }
+        createCards(employeesArr)    
+        }
+      return employeesArr;
+    }
+    getFromLocal()
+
     form.addEventListener("submit",handleSubmit)
     function handleSubmit(event){
         event.preventDefault();
-       let employeeData={
+       let employeesData={
         id:generateId(),
         fullName:event.target.fullname.value,
         department:event.target.departments.value,
         level:event.target.levels.value,
         imgUrl:event.target.imgUrl.value
        };
-       let employee=new Employees(employeeData.id,employeeData.fullName,employeeData.department,employeeData.level,employeeData.imgUrl);
+       let employee=new Employees(employeesData.id,employeesData.fullName,employeesData.department,employeesData.level,employeesData.imgUrl);
        employeesArr.push(employee)
-       console.log(employeesArr)
        addToLocal();
        createCards(employeesArr);
     }
@@ -70,8 +66,9 @@ Employees.prototype.calcSalary=function (level){
     }
     
     
-    function createCards(employeeData){
-        for(let i=0;i<employeeData.length;i++){
+    function createCards(employeesArr){
+        cardsCont.innerHTML=""
+        for(let i=0;i<employeesArr.length;i++){
             let card=document.createElement("div");
             card.classList.add("card");
             let imageCont=document.createElement("div");
@@ -82,12 +79,12 @@ Employees.prototype.calcSalary=function (level){
             let img=document.createElement("img");
             let idCont=document.createElement("p");
             let salaryCont=document.createElement('p');
-            salaryCont.innerText=employeeData[i].salary;
+            salaryCont.innerText=employeesArr[i].salary;
             idCont.innerText=generateId();
-            nameCont.innerText=employeeData[i].name;
-            departmentCont.innerText=employeeData[i].department;
-            levelCont.innerText=employeeData[i].level;
-            img.src=employeeData[i].imgUrl;
+            nameCont.innerText=employeesArr[i].name;
+            departmentCont.innerText=employeesArr[i].department;
+            levelCont.innerText=employeesArr[i].level;
+            img.src=employeesArr[i].imgUrl;
             imageCont.classList.add("image");
             imageCont.appendChild(img);
             cardDescCont.classList.add("card__desc");
